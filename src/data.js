5e5d99244f9947d0e3eca928269fcbe492b1aee0
kparker028 <60025945+kparker028@users.noreply.github.com>
2021-08-11 15:12:51 -0600

    feat: created new data file for boxes

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const boxes = [
+	{
+		status: "Basic",
+		duration: "Month to Month",
+		features: [
+			"large box of toys for your dog/dogs",
+			"One deluxe treat per box",
+		],
+		price: "$15",
+	},
+	{
+		status: "Premier",
+		duration: "Six Months",
+		features: [
+			"larger box of toys for your dog/dogs",
+			"Two deluxe treats every month",
+			"Basic Pet World collar (choose your own color)",
+			"Large mystery gift for your pet",
+		],
+		price: "One Time Fee of $72",
+	},
+	{
+		status: "Platinum",
+		duration: "Twelve Months",
+		features: [
+			"Our largest box of toys (its huge!)",
+			"three deluxe treats every month",
+			"Deluxe Pet World collar w/engraving",
+			"Two Large mystery gifts for you and your pet",
+			"15% discount on all items at PetWorld.com",
+		],
+		price: "One Time Fee of $120",
+	},
+];
